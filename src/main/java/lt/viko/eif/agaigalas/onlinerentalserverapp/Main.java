2f00b93fcc08898bdde8cb01a753eca60da62936
unknown <arnoldasgaigalas@gmail.com>
2024-04-02 11:05:27 +0300

    eddited

--- src/main/java/lt/viko/eif/agaigalas/onlinerentalserverapp/Main.java
+++ src/main/java/lt/viko/eif/agaigalas/onlinerentalserverapp/Main.java
@@ -2,9 +2,6 @@ package lt.viko.eif.agaigalas.onlinerentalserverapp;
 
 import lt.viko.eif.agaigalas.onlinerentalserverapp.meniu.MenuOptions;
 
-import javax.xml.bind.JAXBException;
-
-
 public class Main {
     public static void main(String[] args){
         MenuOptions.menu();
